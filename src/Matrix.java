5a678633b72d619a1855e291338cf7ab59cb2980
AustrianToast <rene.fuhry@gmail.com>
2023-02-28 00:33:17 +0100

    Created importCSV method
    Created the empty method importCSV

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,5 +1,13 @@
+import java.io.BufferedReader;
+
 public class Matrix {
     public static void main(String[] args) {
     
+    }
+
+    public String[] importCSV() {
+        String[] result = null;
+
+        return result;
     }
 }
