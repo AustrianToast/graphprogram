18db15a050a606a554c77d753268acfeb310a55a
AustrianToast <rene.fuhry@gmail.com>
2023-03-05 00:47:41 +0100

    Created TestMatrix class
    For now only has one test for the importCSV method

---
+++ src/TestMatrix.java
@@ -0,0 +1,12 @@
+public class TestMatrix {
+    public static void main(String[] args) {
+        testImportCSV();
+    }
+
+    public static void testImportCSV() {
+        Matrix m = new Matrix();
+
+        m.importCSV("/home/satan/bin/graphprogram/graph.csv");
+        System.out.println(m);
+    }
+}
