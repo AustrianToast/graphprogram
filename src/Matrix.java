f29925a78e3fd74fb12b21401d244e9982173633
AustrianToast <rene.fuhry@gmail.com>
2023-03-05 00:08:13 +0100

    Completed importCSV method
    Added BufferedReader with FileReader surrounded by a try/catch

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,4 +1,7 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Matrix {
     public static void main(String[] args) {
@@ -7,7 +10,22 @@ public class Matrix {
 
     public String[] importCSV() {
         String[] result = null;
+        String line;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(""));
+            
+            line = br.readLine();
 
+            while (line != null) {
+                result = line.trim().split(";");
+            }
+            
+            br.close();
+		} catch (FileNotFoundException e) {
+			e.getStackTrace();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
         return result;
     }
 }
