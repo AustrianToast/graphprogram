99932e227be4241f41f7903fc3045dd546cfc046
René Fuhry <rene.fuhry@gmail.com>
2023-03-17 15:35:53 +0100

    Done, don't ask how

--- src/TestMatrix.java
+++ src/TestMatrix.java
@@ -5,7 +5,13 @@ public class TestMatrix {
 
     public static void test1(String file) {
         Matrix matrix = new Matrix(file);
+        Matrix scalarProduct;
 
         matrix.print();
+        System.out.println();
+        
+        scalarProduct = matrix.multiply( matrix);
+        scalarProduct.print();
+        
     }
 }
